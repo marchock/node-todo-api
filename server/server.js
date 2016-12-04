10bc8b8b2d34b12e1db7601149a88a284c15dc8a
ryan <ryanmarchock@gmail.com>
2016-12-04 01:13:43 +0000

    Test POST

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ var bodyParser = require('body-parser');
 
 
 var {mongoose} = require('./db/mongoose.js');
-var {Todo} = require('./modules/todo.js');
-var {User} = require('./modules/user.js');
+var {Todo} = require('./models/todo.js');
+var {User} = require('./models/user.js');
 
 
 var app = express();
@@ -29,7 +29,7 @@ app.listen(3000, () => {
 })
 
 
-
+module.exports = {app};
 
 
 
