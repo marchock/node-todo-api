843d43176887212d973e2f97efd763e9636c601e
ryan <ryanmarchock@gmail.com>
2016-12-04 23:11:38 +0000

    created test database

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,61 +1,9 @@
 var mongoose = require('mongoose');
 
+
+    console.log('process.env.MONGODB_URI: ', process.env.MONGODB_URI)
+
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
-
-module.exports = {
-    mongoose
-};
-
-
-// var Todo = mongoose.model('Todo', {
-//     text: {
-//         type: String,
-//         required: true,
-//         minlength: 1,
-//         trim: true
-//     },
-//     completed: {
-//         type: Boolean,
-//         default: false
-//     },
-//     completedAt: {
-//         type: Number,
-//         default: null
-//     }
-// });
-//
-//
-// var newTodo = new Todo({
-//     text: '  Edit this video   '
-// });
-//
-// newTodo.save().then((doc) => {
-//     console.log('Save todo', doc)
-// }, (e) => {
-//     console.log('Unable to save', e)
-// });
-
-// var User = mongoose.model('Users', {
-//     email: {
-//         type: String,
-//         required: true,
-//         minlength: 1,
-//         trim: true
-//     }
-// });
-//
-//
-// var newUser = new User({
-//     email: 'ryan@bynd.com'
-// });
-//
-// newUser.save().then((doc) => {
-//     console.log('Saved User', doc)
-// }, (e) => {
-//     console.log('Unable to save user', e)
-// });
-
-
-//save new something
+module.exports = {mongoose};
