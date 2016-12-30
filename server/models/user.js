46ebf08aece68558b0db007e00ceaec6fe76da42
ryan <ryanmarchock@gmail.com>
2016-12-30 14:49:21 +0000

    add get user and authentications

--- server/models/user.js
+++ server/models/user.js
@@ -54,6 +54,27 @@ UserSchema.methods.generateAuthToken = function () {
     });
 };
 
+UserSchema.statics.findByToken = function (token) {
+    var User = this;
+
+    console.log('token; ', token);
+
+    var decoded;
+
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    } catch (e) {
+        return Promise.reject();
+    }
+
+
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    });
+}
+
 var User = mongoose.model('User', UserSchema);
 
 
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
+var {authenticate} = require('./middleware/authenticate.js');
 
 
 var app = express();
@@ -127,6 +128,11 @@ app.post('/user', (req, res) => {
     }).catch((e) => {
         res.status(400).send(e);
     });
+});
+
+
+app.get('/user/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 
